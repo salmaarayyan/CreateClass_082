6ef7e267e050f1709191aa32e70c1eb2ea6f1b08
salmaarayyan <salmaarayyan@gmail.com>
2024-10-30 16:27:41 +0700

    ada file salah

--- soal1.py
+++ soal1.py
@@ -1,11 +1,11 @@
 class PersegiPanjang:
-    def init(self, panjang, lebar): 
+    def __init__(self, panjang, lebar):
         self.panjang = panjang
         self.lebar = lebar
 
     def keliling(self):
         return 2 * (self.panjang + self.lebar)
-
+    
     def luas(self):
         return self.panjang * self.lebar
 
